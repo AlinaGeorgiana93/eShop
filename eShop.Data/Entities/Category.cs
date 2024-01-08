36f762c3310944de503c830a0c43cc91d86428e3
Georgis93 <georgiana.alina93@yahoo.com>
2024-01-08 12:15:45 +0100

    Add project files.

---
+++ eShop.Data/Entities/Category.cs
@@ -0,0 +1,9 @@
+﻿global using eShop.Data.Shared.Interfaces;
+
+namespace eShop.Data.Entities;
+
+public class Category : IEntity
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
